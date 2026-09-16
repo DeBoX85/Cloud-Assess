bf7e86829a8bc09950c2c2975fe711125103bf30
Denis Bogunic <134433647+DeBoX85@users.noreply.github.com>
2026-09-16 17:11:42 +0200

    style: normalize Azure scope adapter formatting

--- internal/discovery/azure_scope.go
+++ internal/discovery/azure_scope.go
@@ -12,7 +12,7 @@ import (
 
 // AzureScopeClient implements subscription and management-group discovery with Azure SDK clients.
 type AzureScopeClient struct {
-	subscriptions     *armsubscription.SubscriptionsClient
+	subscriptions    *armsubscription.SubscriptionsClient
 	managementGroups *armmanagementgroups.ClientFactory
 }
 
@@ -37,7 +37,7 @@ func NewAzureScopeClient(credential azcore.TokenCredential, options *arm.ClientO
 		return nil, fmt.Errorf("create Azure management groups client factory: %w", err)
 	}
 	return &AzureScopeClient{
-		subscriptions:     subscriptions,
+		subscriptions:    subscriptions,
 		managementGroups: managementGroups,
 	}, nil
 }
