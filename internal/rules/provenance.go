6621cc912d89f949d4fb629e7afb2a600a8ec8bb
Denis Bogunic <134433647+DeBoX85@users.noreply.github.com>
2026-09-16 14:20:25 +0200

    style: gofmt rule provenance

--- internal/rules/provenance.go
+++ internal/rules/provenance.go
@@ -2,9 +2,9 @@ package rules
 
 const (
 	ReferenceRepositoryCommit = "8e4f0577f3615e6c9014c031bcad079f235369cc"
-	APRLCommit                 = "60eaddda76541f6adbc1c5ffa686829807e55e29"
-	AORSnapshotTree            = "a3ff1cafbc0a74ea4e4d2cc5aa2812f7c1dab9f5"
-	CustomRulesSnapshotTree    = "674b9b3dcb443ce6dc445b48e1db47e4a0ca7082"
+	APRLCommit                = "60eaddda76541f6adbc1c5ffa686829807e55e29"
+	AORSnapshotTree           = "a3ff1cafbc0a74ea4e4d2cc5aa2812f7c1dab9f5"
+	CustomRulesSnapshotTree   = "674b9b3dcb443ce6dc445b48e1db47e4a0ca7082"
 )
 
 type Provenance struct {
